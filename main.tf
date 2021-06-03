f6c198a7fa3fb9fc668f70c45216eb7077b7c869
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2021-06-03 23:59:10 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "remote" {
     organization = "infrastructure-pipelines-workshop"
     workspaces {
-      name = "john-d-k8s"
+      name = "pratik-k-k8s"
     }
   }
   required_providers {
